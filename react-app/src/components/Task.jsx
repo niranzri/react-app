8026a598e8327be4e0e0df23f6f2e0b8b5f2525d
Marine Le Cleach <lecleach.m@gmail.com>
2023-12-12 18:44:41 +0100

    add tasks

---
+++ react-app/src/components/Task.jsx
@@ -0,0 +1,35 @@
+
+import tasks from '../assets/tasks.json';
+import { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid'
+
+const[tasks, setTasks] = useState(tasks);
+
+function Task(){
+
+return (
+
+<div>
+{tasks.map((currentTask) => {
+return(
+ 
+    <ul>
+       <li>
+         <p>{currentTask.task}</p> 
+         <button>delete</button> 
+         <button onClick={() => clickToDelete()} className="btn-delete"></button>
+        </li> 
+
+    </ul>
+   
+)
+})};
+
+
+</div>
+
+
+)
+
+
+}
