3d2be9052b0b846f12efb3334127941d803b7225
Noelia <noeliairanzoribera@gmail.com>
2023-12-12 17:36:38 +0100

    sidebar

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,23 +1,17 @@
 import { useState } from 'react'
-import classesNav from './styles/navbar.module.css'
-import classesFooter from './styles/footer.module.css'
-import classesSidebar from './styles/sidebar.module.css'
 import ironhackLogo from './assets/ironhack-logo-xs.png';
 import Navbar from './components/Navbar.jsx'
 import Sidebar from './components/Sidebar.jsx'
 import Footer from './components/Footer.jsx'
-
-
 import './App.css'
+
 //it's Marine
 function App() {
 
 
   return (
     <>
-    <Navbar />
     <Sidebar />
-    <Footer />
     </>
   )
 }
--- react-app/src/components/Sidebar.jsx
+++ react-app/src/components/Sidebar.jsx
@@ -1,8 +1,11 @@
-function SideBar (){
+import classes from '../styles/sidebar.module.css'
+
+function SideBar() {
     return (
-        <>
-        
-        </>
+    <div className={classes.sidebar}>
+        <div className={classes.sidebar-div}> <p className={classes.sidebar-text}> Home </p> </div>
+        <div className={classes.sidebar-div}> <p className={classes.sidebar-text}> About</p></div>
+    </div>
     );
 }
 
