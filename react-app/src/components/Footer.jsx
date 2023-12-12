aa1a49f676b247a351b07d7a7e50b155f2d59a84
Marine Le Cleach <lecleach.m@gmail.com>
2023-12-12 17:29:28 +0100

    footer

---
+++ react-app/src/components/Footer.jsx
@@ -0,0 +1,25 @@
+import classesFooter from '../styles/footer.module.css';
+
+function Footer(){
+
+return(
+<>
+
+<footer>
+    <p >
+    This is the footer </p>
+     <a href="https://github.com/niranzri/react-app">link to the repo</a>
+    
+</footer>
+
+
+</>
+
+
+);
+
+
+}
+
+
+export default Footer;
