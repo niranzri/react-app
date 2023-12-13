7da08dcfabf1e1fafd6c675c2dc9c6769c23f1bc
Marine Le Cleach <lecleach.m@gmail.com>
2023-12-13 17:12:25 +0100

    list items

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -3,15 +3,27 @@ import ironhackLogo from './assets/ironhack-logo-xs.png';
 import Navbar from './components/Navbar.jsx'
 import Sidebar from './components/Sidebar.jsx'
 import Footer from './components/Footer.jsx'
+import Task from './components/Task.jsx'
 import './App.css'
 
 
+
+//<Sidebar />
 function App() {
+
+
+
   return (
     <>
+
       <Navbar />
+      
+   <div>
       <Sidebar />
+      <Task />
+   </div>
       <Footer />
+      
     </>
   )
 }
--- react-app/src/components/Task.jsx
+++ react-app/src/components/Task.jsx
@@ -1,28 +1,46 @@
 
-import tasks from '../assets/tasks.json';
+import tasksFromJson from '../assets/tasks.json';
 import { useState } from 'react'
-import { v4 as uuidv4 } from 'uuid'
+import { v4 as uuidv4 } from 'uuid';
+import classes from '../styles/task.module.css'
 
-const[tasks, setTasks] = useState(tasks);
 
+//<button onClick={() => clickToDelete()} className="btn-delete"></button>
 function Task(){
+    const[tasks, setTasks] = useState(tasksFromJson.map((currentTask) => ({task: currentTask.task, id: uuidv4() })));
+    
+   
+    const handleDelete = (id) => {
 
+        const newTasks = tasks.filter((task) =>{
+            return task.id !== id;
+        } ); 
+    
+        setTasks(newTasks);
+    }
+
+
+  
+      
 return (
 
-<div>
-{tasks.map((currentTask) => {
-return(
- 
-    <ul>
-       <li>
-         <p>{currentTask.task}</p> 
-         <button>delete</button> 
-         <button onClick={() => clickToDelete()} className="btn-delete"></button>
-        </li> 
+<div className={classes.toDoList}>
+<h2>Tasks to do</h2>
+{tasks.map(( currentTask) => {
+    return(
         
-    </ul>
+    <ul  key={currentTask.id}>
+        
+       <li className={classes.listItem}>   
+             <p className={classes.itemTask}>{currentTask.task}</p>
+            
+             <button className={classes.btnTask} type='button' onClick={() => handleDelete(currentTask.id)} >
+              Delete
+            </button>
+        </li> 
+    </ul>)
    
-)
+
 })};
 
 
@@ -33,3 +51,5 @@ return(
 
 
 }
+
+export default Task;
