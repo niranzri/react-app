1663648d7ad58c8b5731e136b0f38d05ae8fe371
Noelia <noeliairanzoribera@gmail.com>
2023-12-17 23:21:26 +0100

    removed horizontal line sidebar

--- react-app/src/components/Sidebar.jsx
+++ react-app/src/components/Sidebar.jsx
@@ -6,9 +6,7 @@ function Sidebar() {
     return (
     <div className={classes.sidebar}>
         <NavLink to ='/' className={({ isActive }) => (isActive ? 'active' : '')}> <div className={classes.sidebarDiv}><p className={classes.sidebarText}> Home </p> </div> </NavLink>
-        <div className={classes.horizontalLine}></div>
         <NavLink to ='/AboutPage' className={({ isActive }) => (isActive ? 'active' : '')}> <div className={classes.sidebarDiv}> <p className={classes.sidebarText}> About </p> </div> </NavLink>
-        <div className={classes.horizontalLine}></div>
         <NavLink to ='/TaskHistory' className={({ isActive }) => (isActive ? 'active' : '')}> <div className={classes.sidebarDiv}><p className={classes.sidebarText}> Task History </p> </div> </NavLink>
     </div>
     );
